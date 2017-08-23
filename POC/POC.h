f4e6aa17ba4fb3e2ed2ba900afd4d15c69acbb91
Shweta.G <shwetagoudar7@gmail.com>
2017-08-23 19:42:54 +0530

    initial commit

---
+++ POC/POC.h
@@ -0,0 +1,19 @@
+//
+//  POC.h
+//  POC
+//
+//  Created by Nanda Ballabh on 23/08/17.
+//  Copyright © 2017 My Communication. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for POC.
+FOUNDATION_EXPORT double POCVersionNumber;
+
+//! Project version string for POC.
+FOUNDATION_EXPORT const unsigned char POCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POC/PublicHeader.h>
+
+
---
+++ POC/Webservices.swift
@@ -0,0 +1,65 @@
+//
+//  Webservices.swift
+//  Test
+//
+//  Created by Nanda Ballabh on 23/08/17.
+//  Copyright © 2017 My Communication. All rights reserved.
+//
+
+import UIKit
+
+open class Webservices: NSObject {
+    
+   public static let sharedInstance = Webservices()
+
+    @discardableResult open func faveoDashboardServiceRequest(_ _closure:((NSError?,NSDictionary?) -> Void)? = nil) -> URLSessionDataTask {
+        let urlString : String = "https://rhicluat.religare.com/relinterfacerestful/religare/restful/faveoDashboardService"
+        let url = URL(string: urlString)
+        let json : [String:Any] = ["dashboardInput": ["userId":"20572094","password" : "MTIzNA=="],"masterMetaDataIO":["applicationId": "FAVEO","clientKey": 16148,"token": "token","sessionId": "sessionid","serviceName": "faveoDashboardService","methodName": "faveoDashboardService","requestType": 1,"browser": "chrome","clientIP": "10.216.20.20"]]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("807415", forHTTPHeaderField: "appId")
+        request.setValue("1492507516844", forHTTPHeaderField: "timestamp")
+        request.setValue("lah+w6mfcl1rJEX0l4upNNVYUVS3EOWaOkhkVaiqRvk=", forHTTPHeaderField: "signature")
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            guard let responseData = data else {
+                print("Did not recieve data")
+                return
+            }
+            
+            do {
+                guard let todo = try JSONSerialization.jsonObject(with: responseData, options: []) as? NSDictionary else {
+                    print("No data found")
+                    return
+                }
+                _closure!(nil,todo)
+                print("ToDo Description:", todo)
+                
+                guard let todoTitle = todo["title"] as? String else {
+                    print("Could not get todo title from JSON")
+                    return
+                }
+                print("The title is: " + todoTitle)
+            }
+                
+            catch  {
+                print("error trying to convert data to JSON")
+                return
+            }
+        }
+        task.resume()
+        return task
+        
+    }
+
+}
