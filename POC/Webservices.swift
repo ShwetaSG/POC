7d2ae43bc8ac3f5c052917eb61f0c513ecdda616
Shweta.G <shwetagoudar7@gmail.com>
2017-08-28 17:32:49 +0530

    added common api call.

--- POC/Webservices.swift
+++ POC/Webservices.swift
@@ -62,4 +62,53 @@ open class Webservices: NSObject {
         
     }
 
+    @discardableResult public func commaonApiCall(url: String, request : [String:Any], _ _closure:((NSError?,NSDictionary?) -> Void)? = nil) -> URLSessionDataTask {
+        let urlString : String = url
+        let url = URL(string: urlString)
+        let json : [String:Any] = request
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("807415", forHTTPHeaderField: "appId")
+        request.setValue("1492507516844", forHTTPHeaderField: "timestamp")
+        request.setValue("lah+w6mfcl1rJEX0l4upNNVYUVS3EOWaOkhkVaiqRvk=", forHTTPHeaderField: "signature")
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            guard let responseData = data else {
+                print("Did not recieve data")
+                return
+            }
+            
+            do {
+                guard let todo = try JSONSerialization.jsonObject(with: responseData, options: []) as? NSDictionary else {
+                    print("No data found")
+                    return
+                }
+                _closure!(nil,todo)
+                print("ToDo Description:", todo)
+                
+                guard let todoTitle = todo["title"] as? String else {
+                    print("Could not get todo title from JSON")
+                    return
+                }
+                print("The title is: " + todoTitle)
+            }
+                
+            catch  {
+                print("error trying to convert data to JSON")
+                return
+            }
+        }
+        task.resume()
+        return task
+    }
+
 }
